fbc000e15053ff75b6330c0acb4f1254531132e7
Eduardo Robles Elvira <edulix@wadobo.com>
2015-02-11 20:13:01 +0100

    fixing non-ascii chars error

--- admin/results.sh
+++ admin/results.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # ./results.sh -t tally.tar.gz -c config.json -s
+
+# fixes errors on non-ascii characters
+declare -x LANG="en_GB.UTF-8"
+declare -x LANGUAGE="en_GB:en"
+
 AGORA_RESULTS=/tmp/agora-results
 VENV=/root/.virtualenvs
 source $VENV/agora-results/bin/activate
