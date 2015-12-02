b59b8a4d35b0657647503cbb30c9d50601c1c5a5
Daniel Garcia Moreno <danigm@wadobo.com>
2015-12-02 11:57:51 +0100

    Setting default value to false and existing to true

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,6 +1,7 @@
 # --- !Ups
 
-ALTER TABLE election ADD real boolean NULL DEFAULT null;
+ALTER TABLE election ADD real boolean DEFAULT false;
+UPDATE election set real=true where 1=1;
 
 # --- !Downs
 
