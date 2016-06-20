fad422439f97fc51715a954c1609da07c9cc57cc
Findeton <felrobelv@gmail.com>
2016-06-20 12:55:15 +0200

    update copyright notice years to 2014-2016

--- admin/cycle.py
+++ admin/cycle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # This file is part of agora_elections.
-# Copyright (C) 2014  Agora Voting SL <agora@agoravoting.com>
+# Copyright (C) 2014-2016  Agora Voting SL <agora@agoravoting.com>
 
 # agora_elections is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
--- app/utils/JsonFormatters.scala
+++ app/utils/JsonFormatters.scala
@@ -1,6 +1,6 @@
 /**
  * This file is part of agora_elections.
- * Copyright (C) 2014  Agora Voting SL <agora@agoravoting.com>
+ * Copyright (C) 2014-2016  Agora Voting SL <agora@agoravoting.com>
 
  * agora_elections is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
