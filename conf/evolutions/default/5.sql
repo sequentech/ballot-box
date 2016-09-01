187901dc7ce8c9023121452134f7b87fd8c96347
Findeton <felrobelv@gmail.com>
2016-09-01 10:58:48 +0200

    fix ownership problem with evolutions

--- conf/evolutions/default/5.sql
+++ conf/evolutions/default/5.sql
@@ -18,6 +18,8 @@ SELECT concat('{', string_agg(to_json("key") || ':' || "value", ','), '}')::json
         SELECT "key_to_set", to_json("value_to_set")) AS "fields"
 $function$;;
 
+ALTER FUNCTION json_object_set_key(json, TEXT, anyelement) OWNER TO agora_elections;;
+
 CREATE OR REPLACE FUNCTION "json_object_set_path"(
   "json"          json,
   "key_path"      TEXT[],
@@ -44,54 +46,11 @@ SELECT CASE COALESCE(array_length("key_path", 1), 0)
   FROM array_lower("key_path", 1) l,
        array_upper("key_path", 1) u
 $function$;;
+
+ALTER FUNCTION json_object_set_path(json, TEXT[], anyelement) OWNER TO agora_elections;;
 
 UPDATE election SET configuration=json_object_set_path(configuration::json,'{presentation,share_text}',cast(concat('[{"network":"Twitter","button_text":"","social_message":"', configuration::json->'presentation'->>'share_text', '"}]') AS json));
 
 # --- !Downs
-
-CREATE OR REPLACE FUNCTION "json_object_set_key"(
-  "json"          json,
-  "key_to_set"    TEXT,
-  "value_to_set"  anyelement
-)
-  RETURNS json
-  LANGUAGE sql
-  IMMUTABLE
-  STRICT
-AS $function$
-SELECT concat('{', string_agg(to_json("key") || ':' || "value", ','), '}')::json
-  FROM (SELECT *
-          FROM json_each("json")
-         WHERE "key" <> "key_to_set"
-         UNION ALL
-        SELECT "key_to_set", to_json("value_to_set")) AS "fields"
-$function$;;
-
-CREATE OR REPLACE FUNCTION "json_object_set_path"(
-  "json"          json,
-  "key_path"      TEXT[],
-  "value_to_set"  anyelement
-)
-  RETURNS json
-  LANGUAGE sql
-  IMMUTABLE
-  STRICT
-AS $function$
-SELECT CASE COALESCE(array_length("key_path", 1), 0)
-         WHEN 0 THEN to_json("value_to_set")
-         WHEN 1 THEN "json_object_set_key"("json", "key_path"[l], "value_to_set")
-         ELSE "json_object_set_key"(
-           "json",
-           "key_path"[l],
-           "json_object_set_path"(
-             COALESCE(NULLIF(("json" -> "key_path"[l])::text, 'null'), '{}')::json,
-             "key_path"[l+1:u],
-             "value_to_set"
-           )
-         )
-       END
-  FROM array_lower("key_path", 1) l,
-       array_upper("key_path", 1) u
-$function$;;
 
 UPDATE election SET configuration=json_object_set_path(configuration::json,'{presentation,share_text}',cast(configuration::json#>'{presentation,share_text,0,social_message}' AS json));
