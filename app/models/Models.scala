d1ea89b5c5d6ee4563fca10c7e28106f67d719e4
Eduardo Robles Elvira <edulix@nvotes.com>
2021-04-01 07:59:26 +0200

    adding answer_group_columns_size

--- app/models/Models.scala
+++ app/models/Models.scala
@@ -600,7 +600,7 @@ case class QuestionExtra(
   default_selected_option_ids: Option[Array[Int]],
   select_categories_1click: Option[Boolean],
   answer_columns_size: Option[Int],
-  group_answer_pairs: Option[Boolean],
+  answer_group_columns_size: Option[Int],
   select_all_category_clicks: Option[Int],
   allow_casting_invalid_votes: Option[Boolean], // default = true
   enable_panachage: Option[Boolean], // default = true
@@ -631,6 +631,8 @@ case class QuestionExtra(
     assert(!recommended_preset__deny_text.isDefined || recommended_preset__deny_text.get.length <= LONG_STRING, "recommended_preset__deny_text too long")
 
     assert(!answer_columns_size.isDefined || List(12,6,4,3).contains(answer_columns_size.get), "invalid answer_columns_size, can only be a string with 12,6,4,3")
+
+    assert(!answer_group_columns_size.isDefined || List(12,6,4,3).contains(answer_group_columns_size.get), "invalid answer_group_columns_size, can only be a string with 12,6,4,3")
 
     assert(!(shuffle_all_options.isDefined && shuffle_category_list.isDefined &&
            shuffle_all_options.get) || 0 == shuffle_category_list.get.size,
