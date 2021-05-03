5954314cece11d542680733df0124bfb5b07355b
Eduardo Robles Elvira <edulix@nvotes.com>
2021-05-03 17:23:46 +0200

    adding the review_screen__split_cast_edit flag (#76)

--- app/models/Models.scala
+++ app/models/Models.scala
@@ -766,7 +766,8 @@ case class ElectionExtra(
   success_screen__ballot_ticket__logo_subheader: Option[String],
   success_screen__ballot_ticket__h3: Option[String],
   success_screen__ballot_ticket__h4: Option[String],
-  public_title: Option[String]
+  public_title: Option[String],
+  review_screen__split_cast_edit: Option[Boolean]
 ) {
   def validate() = {
     if (success_screen__redirect_to_login__text.isDefined) 
