76b4937a88a919441ff7eadc3ae61586a089aff8
Eduardo Robles Elvira <edu@sequentech.io>
2022-12-20 12:53:54 +0100

    WIP

--- app/controllers/ElectionsApi.scala
+++ app/controllers/ElectionsApi.scala
@@ -1342,6 +1342,7 @@ object ElectionsApi
                 publicCandidates =          validated.publicCandidates,
                 virtual =                   validated.virtual,
                 tallyAllowed =              validated.tally_allowed,
+                logo_url =                  validated.logo_url,
                 trusteeKeysState =          Some(Json.toJson(trusteeKeysState).toString),
                 segmentedMixing =           validated.segmentedMixing
               )
