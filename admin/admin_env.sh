8a33ea8acf52edc9139bacea534c71469754fc0f
Eduardo Robles <edu@sequentech.io>
2023-01-13 15:28:45 +0100

    Adding support for Fast Segmented Mixing (#151)
    Adding support for Fast Segmented Mixing (#139)
    
    * initial script to segment encrypted ballots
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * missing option in unit test
    
    * missing option in unit test, take II
    
    * missing option in unit test..
    
    * making the config retro-compatible by making segmentedMixing an Option[Boolean] instead of Boolean
    
    * trying to fix build
    
    * trying to fix tests
    
    * fixing limit in category primes calculation

---
+++ admin/admin_env.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This file is part of ballot_box.
+# Copyright (C) 2022  Sequent Tech Inc <legal@sequentech.io>
+
+# ballot_box is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License.
+
+# ballot_box  is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with ballot_box.  If not, see <http://www.gnu.org/licenses/>.
+
+# fixes errors on non-ascii characters
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+
+source /home/ballotbox/env/bin/activate
+$*
