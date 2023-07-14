ac38e2fa0d158d01aa91a3e37a5c2530184ccac8
Findeton <felix@sequentech.io>
2023-07-14 08:04:56 -0500

    wip

--- app/models/Models.scala
+++ app/models/Models.scala
@@ -901,8 +901,10 @@ case class WriteInConfig (
 /** Defines extra fields for write ins */
 case class WriteInField (
   id: String,  // name of the field
-  placeholder: String,
+  placeholder: String, // input text placeholder
   placeholder_i18n: Option[Map[String, String]],
+  label: String, // input text label
+  label_i18n: Option[Map[String, String]],
   min: Int, // minimum 0
   max: Int) // negative if there's no max
 {
